35381d2fa0d68062ee511d06f5fb9e8146d658d9
DruidPro <hataaiee@gmail.com>
2024-01-20 16:45:37 +0500

    settings to ts

--- src/components/SecondPage.tsx
+++ src/components/SecondPage.tsx
@@ -1,6 +1,9 @@
+import NavBar from './navigation/NavBar';
+
 export const SecondPage = () => {
   return (
     <div>
+      <NavBar />
       <h1 className="text-3xl">Component</h1>
       <h1 className="text-3xl">Component</h1>
       <p>sadsadadasdsa</p>
---
+++ src/components/navigation/NavBar.tsx
@@ -0,0 +1,14 @@
+const NavBar = () => {
+  return (
+    <div className="w-screen h-20 bg-orange-500 ">
+      <div className="container my-0 mx-auto justify-between flex items-center h-full">
+        <h1>Navbar</h1>
+        <h1>Navbar</h1>
+        <h1>Navbar</h1>
+        <h1>Navbar</h1>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
