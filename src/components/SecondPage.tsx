564d9777245ff495a26ef3f2083a4b2c19a2b755
DruidPro <hataaiee@gmail.com>
2024-01-27 19:11:08 +0500

    add slider to main page

--- src/components/SecondPage.tsx
+++ src/components/SecondPage.tsx
@@ -1,11 +1,29 @@
+import { useEffect } from 'react';
 import NavBar from './navigation/NavBar';
+import MainSlider from './sliders/MainSlider';
 
 export const SecondPage = () => {
+  // useEffect(() => {
+  //   const kinoboxScript = document.createElement('script');
+  //   kinoboxScript.src = 'https://kinobox.tv/kinobox.min.js';
+  //   kinoboxScript.async = true;
+
+  //   kinoboxScript.onload = () => {
+  //     new window.Kinobox('.kinobox_player', { search: { kinopoisk: '535341' } }).init();
+  //   };
+
+  //   document.body.appendChild(kinoboxScript);
+
+  //   return () => {
+  //     document.body.removeChild(kinoboxScript);
+  //   };
+  // }, []);
   return (
     <div>
       <NavBar />
+      <MainSlider />
       <h1 className="text-3xl">Component</h1>
-      <h1 className="text-3xl">Component</h1>
+      {/* <div className="kinobox_player" style={{ width: '700px' }}></div> */}
       <p>sadsadadasdsa</p>
     </div>
   );
