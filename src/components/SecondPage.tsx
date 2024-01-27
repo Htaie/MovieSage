60817ae0f54911b852aaf713e953449e68028b75
Pasha Rezero <kivachuk.2018@gmail.com>
2024-01-27 16:09:52 +0200

    add login and register

--- src/components/SecondPage.tsx
+++ src/components/SecondPage.tsx
@@ -1,12 +1,15 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import NavBar from './navigation/NavBar';
+import Register from './authComponents/Register';
+import Login from './authComponents/Login';
 
 export const SecondPage = () => {
   return (
     <div>
       <NavBar />
-      <h1 className="text-3xl">Component</h1>
-      <h1 className="text-3xl">Component</h1>
-      <p>sadsadadasdsa</p>
+      <Login />
+      <Register />
     </div>
   );
 };
