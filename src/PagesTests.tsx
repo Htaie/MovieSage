5c125045a1b98774fac44a4066f46c12809be2cd
DruidPro <hataaiee@gmail.com>
2024-02-13 02:21:47 +0500

    settings pre-commit

--- src/PagesTests.tsx
+++ src/PagesTests.tsx
@@ -8,7 +8,6 @@ const PagesTests = () => {
     <div>
       <img
         src="https://avatars.mds.yandex.net/get-ott/1534341/2a00000176f18064fd95abb74cbcc02873b8/orig"
-        src="https://avatars.mds.yandex.net/get-ott/1534341/2a00000176f18064fd95abb74cbcc02873b8/orig"
         alt=""
         style={customShadowStyle}
       ></img>
