6747d17e89899867feb4639f5a40dd8fa0151791
DruidPro <hataaiee@gmail.com>
2024-02-13 02:14:56 +0500

    tyyy

--- src/PagesTests.tsx
+++ src/PagesTests.tsx
@@ -8,6 +8,7 @@ const PagesTests = () => {
     <div>
       <img
         src="https://avatars.mds.yandex.net/get-ott/1534341/2a00000176f18064fd95abb74cbcc02873b8/orig"
+        src="https://avatars.mds.yandex.net/get-ott/1534341/2a00000176f18064fd95abb74cbcc02873b8/orig"
         alt=""
         style={customShadowStyle}
       ></img>
