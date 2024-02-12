7498c672ae711e8f44c2146cdf5151c710268acb
DruidPro <hataaiee@gmail.com>
2024-02-13 02:14:45 +0500

    fix try

--- src/PagesTests.tsx
+++ src/PagesTests.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const PagesTests = () => {
   const customShadowStyle = {
-    filter: 'drop-shadow(20px 20px 6px rgba(0, 0, 0, 0.7))'
-  }
+    filter: 'drop-shadow(20px 20px 6px rgba(0, 0, 0, 0.7))',
+  };
   return (
     <div>
       <img
-        src=https://avatars.mds.yandex.net/get-ott/1534341/2a00000176f18064fd95abb74cbcc02873b8/orig'
-        alt=''
+        src="https://avatars.mds.yandex.net/get-ott/1534341/2a00000176f18064fd95abb74cbcc02873b8/orig"
+        alt=""
         style={customShadowStyle}
       ></img>
     </div>
-  )
-}
+  );
+};
 
-export default PagesTests
+export default PagesTests;
