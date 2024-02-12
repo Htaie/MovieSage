e2431445db5d5cb8dc181888c93d090840874c79
DruidPro <hataaiee@gmail.com>
2024-02-12 19:18:06 +0500

    fixed double rendering for pages

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,4 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './router/router.tsx';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />);
