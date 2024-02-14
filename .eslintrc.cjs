305b06cc0034e1010e1cb23226e9b2ad373561fb
Pasha Rezero <kivachuk.2018@gmail.com>
2024-02-14 11:22:48 +0200

    add hover effect on arrows and shadow

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -44,6 +44,7 @@ module.exports = {
     linkComponents: ['Hyperlink', { name: 'Link', linkAttribute: 'to' }],
   },
   rules: {
+    '@typescript-eslint/semi': 'off',
     'react/jsx-uses-react': 'error',
     'react/jsx-uses-vars': 'error',
     'react/react-in-jsx-scope': 'off',
