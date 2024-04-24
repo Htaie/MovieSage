735ae4fa9f2b8b220731704966c41193e0d1dd96
Pasha Rezero <kivachuk.2018@gmail.com>
2024-04-24 17:05:25 +0300

    add show user's planned list

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,5 +1,5 @@
 import MainSlider from '../../widgets/SlidersForMovie/MainSlider';
-
+import { UserPlanList } from '../../features/ProfileInfo/UserLists/UserPlanList';
 import GenreBlock from '../../widgets/GenreBlock';
 
 export const Home = (): JSX.Element => {
@@ -7,6 +7,7 @@ export const Home = (): JSX.Element => {
     <div className='h-full bg-[#212124] '>
       <div className='container mx-auto'>
         <MainSlider />
+        <UserPlanList />
         <GenreBlock />
       </div>
     </div>
