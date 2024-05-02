4cac5df4dd05e7160c4448ba2a0d9bb3579ce4a2
Pasha Rezero <kivachuk.2018@gmail.com>
2024-05-02 17:41:03 +0300

    change some style and add icon for show buttons to add in planned and watched lists

--- src/features/MovieDetails/RatingStar.tsx
+++ src/features/MovieDetails/RatingStar.tsx
@@ -8,6 +8,9 @@ import { supabase } from '../../../backend/apiClient/client.js';
 import { userDataStore } from '../../shared/store/UserStore';
 import { useStore } from 'effector-react';
 import { v4 as uuidv4 } from 'uuid';
+import AddRoundedIcon from '@mui/icons-material/AddRounded';
+import { motion } from 'framer-motion';
+
 interface RaitingInfoProps {
   data: MovieType;
 }
@@ -75,6 +78,7 @@ export const RaitingInfo = ({ data }: RaitingInfoProps): JSX.Element => {
   const [ratedList, setRatedList] = useState<any[]>([]);
   const userId = dataUserId;
   const uniqueId = uuidv4();
+  const [isIconRotated, setIsIconRotated] = useState(false);
 
   const stars = Array.from({ length: 10 }, (_, index) => (
     <StarOutlinedIcon key={index} style={{ fontSize: '3em', color: '#a0a0a0' }} />
@@ -113,6 +117,7 @@ export const RaitingInfo = ({ data }: RaitingInfoProps): JSX.Element => {
     if (ratedListData) {
       userRatingStore.setState(ratedListData);
     }
+    alert('Movie added to liked list');
   };
 
   const handleAddToPlanList = async () => {
@@ -122,6 +127,7 @@ export const RaitingInfo = ({ data }: RaitingInfoProps): JSX.Element => {
     if (plannedListData) {
       userPlanListStore.setState(plannedListData);
     }
+    alert('Movie added to planned list');
   };
 
   const fetchUserList = async (listName: string) => {
@@ -189,21 +195,52 @@ export const RaitingInfo = ({ data }: RaitingInfoProps): JSX.Element => {
               );
             })}
           </div>
-          <div className='flex justify-between'>
-            <p className='font-bold text-5xl'>{ratingScore}</p>
+          <div className='flex'>
+            <p className='font-bold text-5xl mr-6'>{ratingScore}</p>
             {userRating ? (
-              <div className='flex'>
-                <p className='font-bold text-4xl mr-[20px]'>Ваша оценка:</p>
-                <span className='font-bold text-5xl'>{userRating}</span>
+              <div className='flex items-center'>
+                <p className='font-bold text-xl mr-2'>Ваша оценка:</p>
+                <span className='font-bold text-xl'>{userRating}</span>
               </div>
             ) : null}
-            <MainBtn
-              text={'Добавить в запланированные'}
-              onClick={() => {
-                handleAddToPlanList();
-              }}
-              style={{ marginBottom: '20px' }}
-            />
+            <motion.div
+              className='absolute ml-[280px] z-10'
+              onClick={() => setIsIconRotated((prevState) => !prevState)}
+              animate={{ rotate: isIconRotated ? 45 : 0 }}
+              transition={{ duration: 0.3 }}
+            >
+              <AddRoundedIcon
+                style={{ fontSize: '55px' }}
+                className={`${isIconRotated ? '' : 'bg-[#272724] text-[#5138E9]'} rounded-[30px] hover:cursor-pointer hover:scale-105`}
+              />
+            </motion.div>
+            {isIconRotated && (
+              <motion.div
+                className='absolute bg-[#5138E9] ml-[280px] w-[340px] h-[105px] rounded-[20px]'
+                initial={{ scaleY: 0, opacity: 0, y: -50 }}
+                animate={{ scaleY: isIconRotated ? 1 : 0, opacity: isIconRotated ? 1 : 0, y: isIconRotated ? 0 : -50 }}
+                transition={{ duration: 0.3 }}
+              >
+                <div className='flex flex-col'>
+                  <MainBtn
+                    text={'Добавить в просмотренные'}
+                    onClick={() => {
+                      handleStarClick(0);
+                      setIsIconRotated(false);
+                    }}
+                    style={{ scale: '0.7' }}
+                  />
+                  <MainBtn
+                    text={'Добавить в запланированные'}
+                    onClick={() => {
+                      handleAddToPlanList();
+                      setIsIconRotated(false);
+                    }}
+                    style={{ scale: '0.7' }}
+                  />
+                </div>
+              </motion.div>
+            )}
           </div>
         </div>
       </div>
