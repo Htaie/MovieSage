82d4637bb0c60d578b7d98fd34518b69a2111724
Pasha Rezero <kivachuk.2018@gmail.com>
2024-05-03 16:11:47 +0300

    add scroll progress animation and fix modal trailer position and add autoplay

--- src/features/MovieDetails/TrailerModal.tsx
+++ src/features/MovieDetails/TrailerModal.tsx
@@ -1,7 +1,7 @@
 const TrailerModal = ({ trailer }: { trailer: any }): JSX.Element => {
   return (
     <div style={{ textAlign: 'center' }}>
-      <iframe width='1200' height='800' src={trailer}></iframe>
+      <iframe width='1200' height='600' src={`${trailer}?autoplay=1`} allow='autoplay'></iframe>
     </div>
   );
 };
