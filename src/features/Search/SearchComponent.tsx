33fb141e9b4e622fe276ccf8b325b0b4676d61aa
Pasha Rezero <kivachuk.2018@gmail.com>
2024-05-07 16:57:02 +0300

    update component's styles and add results component

--- src/features/Search/SearchComponent.tsx
+++ src/features/Search/SearchComponent.tsx
@@ -1,12 +1,21 @@
 import SearchIcon from '@mui/icons-material/Search';
+import { SearchResults } from './SearchResults';
 import { useState } from 'react';
 import { motion } from 'framer-motion';
 
 export const SearchComponent = () => {
   const [showSearchInput, setShowSearchInput] = useState(false);
+  const [searchValue, setSearchValue] = useState('');
+  const [showSearchResults, setShowSearchResults] = useState(false);
 
   const toggleSearchInput = () => {
     setShowSearchInput(!showSearchInput);
+  };
+
+  const handleInputChange = (event: any) => {
+    const value = event.target.value;
+    setSearchValue(value);
+    setShowSearchResults(value.length > 0);
   };
 
   const inputVariants = {
@@ -15,19 +24,28 @@ export const SearchComponent = () => {
   };
 
   return (
-    <div className='flex items-center relative w-[330px]' style={{ overflow: 'hidden' }}>
-      <motion.div animate={showSearchInput ? 'open' : 'closed'} variants={inputVariants} transition={{ duration: 0.5 }}>
-        <input
-          type='text'
-          placeholder='Поиск'
-          className='text-black w-[300px] h-[35px] rounded-[4px] outline-none pl-10'
-        ></input>
-      </motion.div>
-      <SearchIcon
-        style={{ position: 'relative', left: showSearchInput ? '-300px' : '0' }}
-        className={`${showSearchInput ? 'text-black' : ''} ml-2 hover:cursor-pointer hover:text-[#5138E9]`}
-        onClick={toggleSearchInput}
-      />
+    <div className='flex'>
+      <div className='flex items-center justify-start relative w-[330px]' style={{ overflow: 'hidden' }}>
+        <motion.div
+          animate={showSearchInput ? 'open' : 'closed'}
+          variants={inputVariants}
+          transition={{ duration: 0.5 }}
+        >
+          <input
+            type='text'
+            placeholder='Поиск'
+            className='text-black w-[330px] h-[35px] rounded-[4px] outline-none pl-10'
+            value={searchValue}
+            onChange={handleInputChange}
+          ></input>
+        </motion.div>
+        <SearchIcon
+          style={{ position: 'relative', left: showSearchInput ? '-330px' : '-30px' }}
+          className={`${showSearchInput ? 'text-black' : ''} ml-2 hover:cursor-pointer hover:text-[#5138E9]`}
+          onClick={toggleSearchInput}
+        />
+      </div>
+      {showSearchResults && <SearchResults />}
     </div>
   );
 };
---
+++ src/features/Search/SearchResults.tsx
@@ -0,0 +1,7 @@
+export const SearchResults = () => {
+  return (
+    <div className='ml-[-330px]'>
+      <div className='bg-[#242424] text-white w-[330px] h-[250px] rounded-b-xl mt-[280px]'>Результаты поиска</div>
+    </div>
+  );
+};
