0f2dba650bfe7b504d877abc1dcf08e9b3818652
Pasha Rezero <kivachuk.2018@gmail.com>
2024-05-13 16:52:04 +0300

    update main slider

--- src/widgets/SlidersForMovie/MainSlider.tsx
+++ src/widgets/SlidersForMovie/MainSlider.tsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, Pagination } from 'swiper/modules';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css';
@@ -10,6 +8,7 @@ import { TOKEN, API_URL } from '../../shared/constants/constants';
 import { MovieType } from '../../shared/types/MoviesTypes';
 import GenreLink from '../../features/GenreLink/GenreLink';
 import styles from './MainSliderStyles.module.css';
+import { motion } from 'framer-motion';
 
 const MainSlider: React.FC = () => {
   const divStyle = {
@@ -19,8 +18,8 @@ const MainSlider: React.FC = () => {
   };
 
   const [data, setData] = useState<MovieType[]>([]);
-  const [activeSlideIndex, setActiveSlideIndex] = useState<number>(0);
-  const [activeMovieId, setActiveMovieId] = useState<number | null>(null);
+  const [randomMovie, setRandomMovie] = useState<any>(null);
+
   useEffect(() => {
     const fetchData = async (): Promise<void> => {
       try {
@@ -47,74 +46,78 @@ const MainSlider: React.FC = () => {
     };
     void fetchData();
   }, []);
+
+  const getRandomMovie = (movies: any) => {
+    const keys = Object.keys(movies);
+    const randomKey = keys[Math.floor(Math.random() * keys.length)];
+    return movies[randomKey];
+  };
+
   useEffect(() => {
-    if (data.length > 0) {
-      setActiveMovieId(data[0]?.id);
-    }
+    const intervalId = setInterval(() => {
+      setRandomMovie(getRandomMovie(data));
+    }, 5000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
   }, [data]);
 
+  const animationVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { duration: 3 } },
+  };
+
+  console.log(randomMovie);
+
   return (
-    <Swiper
-      loop={true}
-      // autoplay={{
-      //   delay: 2500,
-      //   disableOnInteraction: false,
-      // }}
-      slidesPerView={'auto'}
-      pagination={{
-        clickable: true,
-      }}
-      navigation={true}
-      modules={[Pagination, Autoplay]}
-      className=' container  w-[100%]  bg-[#1C3334] mt-20  swiper-navigation-color swiper-pagination-color rounded-3xl'
-      onSlideChangeTransitionEnd={(swiper) => {
-        setActiveSlideIndex(swiper.realIndex);
-        setActiveMovieId(data[swiper.realIndex]?.id);
-      }}
-    >
-      {data.slice(0, 5).map(
-        (movie: MovieType) =>
-          movie.backdrop.url && ( //чмошники на kinopoisk.dev не приходит лого поэтому я пока поставил backdrop :(
-            <SwiperSlide key={movie.id}>
-              <div className='relative'>
-                <img
-                  className='h-full w-full object-cover'
-                  src={movie.backdrop.url || 'https://placehold.co/1000x500'}
-                  loading='lazy'
-                  alt='backdropMovie'
-                />
-                {activeMovieId === movie.id && (
-                  <div
-                    className={`${styles['slide-from-left']} absolute bottom-12 left-0 ml-40 font-bold text-white z-10`}
-                  >
-                    {/* <img
-                    className='h-[120px] w-[50%] ml-2 main-slider'
-                    src={movie.logo.url}
-                    alt={movie.name}
-                    style={customShadowStyle}
-                  /> */}
-                    <div className='flex items-center space-x-2 mt-[170px] ml-2 mb-2'>
-                      <p>{movie.rating.imdb} IMDB</p>
-                      <p>{RatingRounding(movie.rating.kp)} KINOPOISK</p>
-                    </div>
-                    <div className='flex flex-wrap w-[70%]'>
-                      <GenreLink genres={movie.genres} />
-                    </div>
-                    <h1 className='w-[460px] text-wrap text-xl mx-2 mt-3 mb-10'>{movie.shortDescription}</h1>
-                    <Link to={`/movie/${movie.id}`} className='py-4 px-7  text-black bg-white rounded-3xl'>
-                      Перейти к фильму
-                    </Link>
-                  </div>
-                )}
-                <div
-                  className='absolute w-[700px] h-[450px] bottom-0 left-12 bg-black opacity-50 rounded-full'
-                  style={divStyle}
-                ></div>
-              </div>
-            </SwiperSlide>
-          )
+    <>
+      {randomMovie && (
+        <div
+          key={data.indexOf(randomMovie)}
+          className='relative'
+          style={{
+            backgroundImage: `https://placehold.co/1540x786`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            minHeight: '786px',
+          }}
+        >
+          <motion.img
+            variants={animationVariants}
+            initial='hidden'
+            animate='visible'
+            className='h-full w-full object-cover'
+            src={randomMovie.backdrop.url || 'https://placehold.co/1000x500'}
+            loading='lazy'
+            alt='backdropMovie'
+          />
+          <motion.div
+            variants={animationVariants}
+            initial='hidden'
+            animate='visible'
+            className='absolute bottom-12 left-0 ml-40 font-bold text-white z-10'
+          >
+            {/* <img className='h-[120px] w-[50%] ml-2 main-slider' src={randomMovie.logo.url} alt={randomMovie.name} /> */}
+            <div className='flex items-center space-x-2 mt-[170px] ml-2 mb-2'>
+              <p>{randomMovie.rating.imdb} IMDB</p>
+              <p>{RatingRounding(randomMovie.rating.kp)} KINOPOISK</p>
+            </div>
+            <div className='flex flex-wrap w-[70%]'>
+              <GenreLink genres={randomMovie.genres} />
+            </div>
+            <h1 className='w-[460px] text-wrap text-xl mx-2 mt-3 mb-10'>{randomMovie.shortDescription}</h1>
+            <Link to={`/movie/${randomMovie.id}`} className='py-4 px-7  text-black bg-white rounded-3xl'>
+              Перейти к фильму
+            </Link>
+          </motion.div>
+          <div
+            className='absolute w-[700px] h-[450px] bottom-0 left-12 bg-black opacity-50 rounded-full'
+            style={divStyle}
+          ></div>
+        </div>
       )}
-    </Swiper>
+    </>
   );
 };
 
