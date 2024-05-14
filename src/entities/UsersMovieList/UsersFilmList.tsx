969e2b3e3670203a2b26360b2b8269126aa23bfb
Pasha Rezero <kivachuk.2018@gmail.com>
2024-05-14 22:34:33 +0300

    add button for open search modal

--- src/entities/UsersMovieList/UsersFilmList.tsx
+++ src/entities/UsersMovieList/UsersFilmList.tsx
@@ -18,6 +18,7 @@ import { PROFILE_ROUTE } from '../../shared/constants/constants';
 import { userDataStore } from '../../shared/store/UserStore';
 import { supabase } from '../../../backend/apiClient/client.js';
 import { CustomPagination } from '../../features/Pagination/CustomPagination.js';
+import AddRoundedIcon from '@mui/icons-material/AddRounded';
 
 export const UsersFilmsList = ({ formType }: { formType: string }) => {
   const data = formType === PROFILE_ROUTE.RATED ? useStore(userRatingStore) : useStore(userPlanListStore);
@@ -30,6 +31,7 @@ export const UsersFilmsList = ({ formType }: { formType: string }) => {
   const [selectedRating, setSelectedRating] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 10;
+  const [searchModal, setSearchModal] = useState(false);
 
   const handleMouseEnter = (
     {
@@ -201,71 +203,86 @@ export const UsersFilmsList = ({ formType }: { formType: string }) => {
     setCurrentPage(page);
   };
 
+  const openSearchModal = () => {
+    setSearchModal(true);
+  };
+
   return (
-    <div className='flex flex-col'>
-      <div className='text-white border-2 border-solid border-[#5138E9] rounded-lg h-[582px] w-[80%] pb-[400px] mx-auto mt-[30px] mb-[30px]'>
-        {Object.keys(data).length === 0 ? (
-          <div className='flex items-center mb-2'>
-            <div className='flex-1 flex flex-col items-center'>
-              <SentimentDissatisfiedIcon style={{ fontSize: 200 }} />
-              <p className='text-xl ml-2'>Брадочек,у тебя нет фильмов</p>
-            </div>
-          </div>
-        ) : (
-          currentItems.map((movie_id, index) => {
-            const film = data[movie_id];
-            return (
-              <div key={movie_id} className='flex bg-[#45475B] h-[50px] items-center my-[7px] mx-[7px]'>
-                <div className='flex-1 flex ml-4'>
-                  <Link
-                    to={`/movie/${film.movie_id}`}
-                    className='text-xl mr-4'
-                    onMouseEnter={(event) =>
-                      handleMouseEnter(film, event as unknown as MouseEvent<HTMLAnchorElement, MouseEvent>)
-                    }
-                    onMouseLeave={handleMouseLeave}
-                  >
-                    {film.title}
-                  </Link>
-                  <p className='text-xl'>{film.year}</p>
-                </div>
-                <div className='flex-1 flex justify-end items-center mr-4'>
-                  <EditIcon />
-                  <select
-                    className='text-xl bg-[#45475B] appearance-none pl-[10px] w-[40px] py-2 mr-2 focus:outline-none'
-                    value={film.clicked_rating}
-                    onChange={(event) => handleRatingChange(film.movie_id, parseInt(event.target.value), index)}
-                  >
-                    {[...Array(10)].map((_, index) => (
-                      <option key={index} value={index + 1}>
-                        {index + 1}
-                      </option>
-                    ))}
-                  </select>
-                  <p className='text-xl'>{film.type}</p>
-                  <DeleteForeverIcon
-                    onClick={() => {
-                      handleDeleteFilm(film.movie_id, index);
-                    }}
-                    className='text-3xl ml-4 cursor-pointer hover:text-red-500'
-                  ></DeleteForeverIcon>
-                </div>
+    <>
+      {formType === PROFILE_ROUTE.RATED && (
+        <div
+          onClick={openSearchModal}
+          className='flex justify-center items-center text-white bg-[#5138E9] h-[30px] w-[300px] hover:cursor-pointer rounded-lg ml-[195px] mt-2'
+        >
+          <p>Добавить просмотренные фильмы</p>
+          <AddRoundedIcon />
+        </div>
+      )}
+      <div className='flex flex-col'>
+        <div className='text-white border-2 border-solid border-[#5138E9] rounded-lg h-[582px] w-[80%] pb-[400px] mx-auto mt-[30px] mb-[30px]'>
+          {Object.keys(data).length === 0 ? (
+            <div className='flex items-center mb-2'>
+              <div className='flex-1 flex flex-col items-center'>
+                <SentimentDissatisfiedIcon style={{ fontSize: 200 }} />
+                <p className='text-xl ml-2'>Брадочек,у тебя нет фильмов</p>
               </div>
-            );
-          })
-        )}
-        <MovieModal
-          modalData={modalData}
-          isHovered={isHovered}
-          linkPosition={linkPosition}
-          setIsHovered={setIsHovered}
-          currentLink={currentLink}
-          formType={formType}
-          addToPlanList={() => handleAddToPlanList(modalData)}
-          addToRatedList={() => handleAddToRatedList(modalData)}
-        />
+            </div>
+          ) : (
+            currentItems.map((movie_id, index) => {
+              const film = data[movie_id];
+              return (
+                <div key={movie_id} className='flex bg-[#45475B] h-[50px] items-center my-[7px] mx-[7px]'>
+                  <div className='flex-1 flex ml-4'>
+                    <Link
+                      to={`/movie/${film.movie_id}`}
+                      className='text-xl mr-4'
+                      onMouseEnter={(event) =>
+                        handleMouseEnter(film, event as unknown as MouseEvent<HTMLAnchorElement, MouseEvent>)
+                      }
+                      onMouseLeave={handleMouseLeave}
+                    >
+                      {film.title}
+                    </Link>
+                    <p className='text-xl'>{film.year}</p>
+                  </div>
+                  <div className='flex-1 flex justify-end items-center mr-4'>
+                    <EditIcon />
+                    <select
+                      className='text-xl bg-[#45475B] appearance-none pl-[10px] w-[40px] py-2 mr-2 focus:outline-none'
+                      value={film.clicked_rating}
+                      onChange={(event) => handleRatingChange(film.movie_id, parseInt(event.target.value), index)}
+                    >
+                      {[...Array(10)].map((_, index) => (
+                        <option key={index} value={index + 1}>
+                          {index + 1}
+                        </option>
+                      ))}
+                    </select>
+                    <p className='text-xl'>{film.type}</p>
+                    <DeleteForeverIcon
+                      onClick={() => {
+                        handleDeleteFilm(film.movie_id, index);
+                      }}
+                      className='text-3xl ml-4 cursor-pointer hover:text-red-500'
+                    ></DeleteForeverIcon>
+                  </div>
+                </div>
+              );
+            })
+          )}
+          <MovieModal
+            modalData={modalData}
+            isHovered={isHovered}
+            linkPosition={linkPosition}
+            setIsHovered={setIsHovered}
+            currentLink={currentLink}
+            formType={formType}
+            addToPlanList={() => handleAddToPlanList(modalData)}
+            addToRatedList={() => handleAddToRatedList(modalData)}
+          />
+        </div>
+        <CustomPagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
       </div>
-      <CustomPagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
-    </div>
+    </>
   );
 };
