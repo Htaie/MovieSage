092f97695cc4b4e5106432d930c40beddac0e0f4
Pasha Rezero <kivachuk.2018@gmail.com>
2024-11-05 15:32:18 +0200

    update save to local storage logic

--- src/entities/SlidersForGenres/FilmsByGenreSlider.tsx
+++ src/entities/SlidersForGenres/FilmsByGenreSlider.tsx
@@ -13,26 +13,33 @@ export const FilmByGenreSlider = ({ genre }: { genre: string; isMobile: boolean 
       const url = `
       https://api.kinopoisk.dev/v1.4/movie?page=1&limit=10&selectFields=&notNullFields=poster.url&sortField=votes.imdb&sortType=1&year=2024&rating.imdb=5-10&genres.name=${genre}`;
       const movieStoredData = localStorage.getItem(`movieByGenresData_${genre}`);
+      const lastDataUpdate = localStorage.getItem(`lastUpdateTime_${genre}`);
+      const now = new Date().getTime();
       try {
-        if (movieStoredData) {
-          setData(JSON.parse(movieStoredData));
-        } else {
-          const response = await fetch(url, {
-            method: 'GET',
-            headers: {
-              Accept: 'application/json',
-              'X-API-KEY': TOKEN,
-            },
-          });
-
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
+        if (movieStoredData && lastDataUpdate) {
+          const storedDate = new Date(lastDataUpdate).getTime();
+          const isUpdateNeeded = now - storedDate > 1000 * 60 * 60 * 24;
+          if (!isUpdateNeeded) {
+            setData(JSON.parse(movieStoredData));
+            return;
           }
-
-          const responseData = await response.json();
-          setData(responseData.docs);
-          localStorage.setItem(`movieByGenresData_${genre}`, JSON.stringify(responseData.docs));
         }
+        const response = await fetch(url, {
+          method: 'GET',
+          headers: {
+            Accept: 'application/json',
+            'X-API-KEY': TOKEN,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const responseData = await response.json();
+        setData(responseData.docs);
+        localStorage.setItem(`movieByGenresData_${genre}`, JSON.stringify(responseData.docs));
+        localStorage.setItem(`lastUpdateTime_${genre}`, new Date().toISOString());
       } catch (error) {
         console.error('There was a problem with the fetch operation:', error);
       }
@@ -42,7 +49,7 @@ export const FilmByGenreSlider = ({ genre }: { genre: string; isMobile: boolean 
 
   return (
     <>
-      <MovieSlider data={data} title={genre}/>
+      <MovieSlider data={data} title={genre} />
     </>
   );
 };
